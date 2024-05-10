6a92b6e10a906d5610bae53375e78285c28d375b
sushant007abd <93302654+sushant007abd@users.noreply.github.com>
2024-05-10 23:38:20 +0530

    production ready

--- springboot-backend/src/main/java/com/omnilearn/springbootbackend/controller/UserController.java
+++ springboot-backend/src/main/java/com/omnilearn/springbootbackend/controller/UserController.java
@@ -18,7 +18,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://omnilearn.vercel.app")
 public class UserController {
 
     @Autowired
